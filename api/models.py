f25daa39b09970d44370a53e61f56d2f530583b4
web-weebie <twinklet969@gmail.com>
2023-09-12 23:39:26 +0100

    Feature: Initial commit, project setup

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=250, blank=False)
+    email = models.CharField(max_length=250, blank=False)
+    username = models.CharField(max_length=200, blank=False)
+    age = models.PositiveIntegerField
+    track = models.CharField(max_length=150, blank=True)
+    occupation = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return str(self.name)
